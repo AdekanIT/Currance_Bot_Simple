273345c69995f20395bdcec0292a50bb6d2de6bf
AdekanIT <adekankain@gmail.com>
2024-01-02 10:54:29 +0500

    Add count

--- main.py
+++ main.py
@@ -24,12 +24,42 @@ def  currency(message):
     user_id = message.from_user.id
     if message.text.upper() == 'RUB':
         bot.send_message(user_id, '1 RUB is 138,64 SOM in 01.01.2024', reply_markup=ReplyKeyboardRemove())
+        bot.send_message(user_id, 'How many RUB you wanna take?', reply_markup=ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, math_rub)
     elif message.text.upper() == 'EUR':
         bot.send_message(user_id, '1 EUR is 13 673,18 SOM in 01.01.2024', reply_markup=ReplyKeyboardRemove())
+        bot.send_message(user_id, 'How many EUR you wanna take?', reply_markup=ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, math_eur)
     elif message.text.upper() == 'USD':
         bot.send_message(user_id, '1 USD is 12 373,36 SOM in 01.01.2024', reply_markup=ReplyKeyboardRemove())
+        bot.send_message(user_id, 'How many USD you wanna take?', reply_markup=ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, math_usd)
     else:
         bot.send_message(user_id, 'WTF?!')
+
+
+def math_rub(message):
+    user_id = message.from_user.id
+    num = float(message.text)
+    som = num * 138.64
+    som = round(som, 2)
+    bot.send_message(user_id, f'{som} SOM cost of {num} RUB!', reply_markup=ReplyKeyboardRemove())
+
+
+def math_eur(message):
+    user_id = message.from_user.id
+    num = float(message.text)
+    som = num * 13673.18
+    som = round(som, 2)
+    bot.send_message(user_id, f'{som} SOM cost of {num} EUR!', reply_markup=ReplyKeyboardRemove())
+
+
+def math_usd(message):
+    user_id = message.from_user.id
+    num = float(message.text)
+    som = num * 12373.36
+    som = round(som, 2)
+    bot.send_message(user_id, f'{som} SOM cost of {num} USD!', reply_markup=ReplyKeyboardRemove())
 
 
 bot.polling(none_stop=True)
